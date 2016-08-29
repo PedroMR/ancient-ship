e0c3381a1d2f6a297d0a9d3fe58fbe90145f36b1
Pedro Rabinovitch <pedromr@gmail.com>
2016-08-29 15:01:18 -0400

    Initial commit

---
+++ Assets/AncientMech/Scripts/ActivateOnInput.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateOnInput : MonoBehaviour {
+	public Behaviour[] TargetComponents;
+	public GameObject[] TargetObjects;
+	public KeyCode KeyToActivate;
+	public bool ActivateWhileHolding = true;
+
+	private bool active = false;
+
+	// Update is called once per frame
+	void Update () {
+		if (ActivateWhileHolding) {
+			if (TargetComponents != null) {
+				foreach(var component in TargetComponents)
+					component.enabled = Input.GetKey(KeyToActivate);
+			}
+			if (TargetObjects != null) {
+				foreach(var gameObject in TargetObjects)
+					gameObject.SetActive(Input.GetKey(KeyToActivate));
+			}
+		} else {
+			if (Input.GetKeyDown(KeyToActivate)) {
+//					TargetComponent.enabled = !TargetComponent.enabled;
+			}
+		}
+	}
+}
---
+++ Assets/AncientMech/Scripts/RandomizeInputs.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class RandomizeInputs : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start()
+	{
+
+		var inputComponents = GetComponentsInChildren<ActivateOnInput>();
+
+		KeyCode[] keys = {
+			KeyCode.A,
+			KeyCode.Q,
+			KeyCode.W,
+			KeyCode.S,
+			KeyCode.Z,
+			KeyCode.X,
+			KeyCode.E,
+			KeyCode.D,
+			KeyCode.C
+		};
+
+		var availableKeys = new List<KeyCode>(keys);
+
+		foreach(var component in inputComponents) {
+			var index = Random.Range(0, availableKeys.Count);
+			component.KeyToActivate = availableKeys[index];
+			availableKeys.RemoveAt(index);
+
+			if (availableKeys.Count == 0) {
+				availableKeys.AddRange(keys);
+			}
+		}
+	
+	}
+
+}
+
