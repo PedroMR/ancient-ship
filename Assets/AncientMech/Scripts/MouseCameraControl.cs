02eaed53e3f58a66952f4f8bff0ddfd2c293b8af
Pedro Rabinovitch <pedromr@gmail.com>
2016-08-29 17:54:36 -0400

    Improving mouse camera control -- added zoom

---
+++ Assets/AncientMech/Scripts/MouseCameraControl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseCameraControl : MonoBehaviour
+{
+	public Transform target;
+	public float MaxZ = 100.0f;
+	public float MinZ = 10.0f;
+	public float ZChangePerWheel = 1f;
+
+	public float zOffset = 20.0f;
+
+
+	private void LateUpdate()
+	{
+		zOffset += Input.mouseScrollDelta.y * ZChangePerWheel * Time.deltaTime;
+			
+		zOffset = Mathf.Clamp(zOffset, MinZ, MaxZ);
+
+		transform.position = target.position + Vector3.back * zOffset;
+	}
+}
+
