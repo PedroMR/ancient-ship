ab9b61c0903c5fa08a336036de6a3ba79740f4c5
Pedro Rabinovitch <pedromr@gmail.com>
2016-08-31 15:36:00 -0400

    Updated to new Unity version. Added landing pad sensor

---
+++ Assets/AncientMech/Scripts/LandingPad.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LandingPad : MonoBehaviour
+{
+	public enum Effect {
+		WinLevel
+	};
+
+	public Effect effect;
+	public float TimeRequired = 3;
+
+	private float timeInTrigger;
+	public bool Triggered = false;
+
+	public delegate void Action(LandingPad caller);
+	public Action WasTriggered;
+
+	// Use this for initialization
+	void Start()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+	
+	}
+
+	void OnTriggerExit() {
+		timeInTrigger = 0;
+	}
+
+	void OnTriggerStay() {
+		timeInTrigger += Time.deltaTime;
+
+		if (timeInTrigger >= TimeRequired) {
+			if (WasTriggered != null)
+				WasTriggered(this);
+			Triggered = true;
+		}
+	}
+}
+
