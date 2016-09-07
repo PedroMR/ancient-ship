296d878e8b0767e2a7dfa4de4a91f39c3c116d97
Pedro Rabinovitch <pedromr@gmail.com>
2016-09-07 17:36:44 -0400

    Buildings explode. BOOM

---
+++ Assets/AncientMech/Scripts/ExplodesOnContact.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplodesOnContact : MonoBehaviour
+{
+	public GameObject Explosion;
+	public float PointValue = 1;
+	public float DestructionTimer = 0.3f;
+
+	private bool dead = false;
+
+	// Use this for initialization
+	void Start()
+	{
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (dead)
+			return;
+
+		if (other.gameObject.layer != LayerMask.NameToLayer("Player"))
+			return;
+		
+		if (Explosion != null) {
+			var newExplosion = Instantiate(Explosion, transform.position, transform.rotation);
+
+		}
+		dead = true;
+		Destroy(gameObject, DestructionTimer);
+	}
+}
+
