88d17a761b5c232b8838f79706c84280d33e37b8
Pedro Rabinovitch <pedromr@gmail.com>
2016-09-12 17:38:44 -0400

    Connecting via AirConsole. It's a bit magical.

---
+++ Assets/AncientMech/Scripts/AirConsoleMapper.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
+
+public class AirConsoleMapper : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start()
+	{
+		AirConsole.instance.onMessage += OnMessage;
+		AirConsole.instance.onConnect += OnConnect;
+	}
+
+	void OnConnect (int device_id)
+	{
+		Debug.Log("OnConnect " + device_id);
+		AirConsole.instance.SetActivePlayers();
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+	
+	}
+
+	void OnMessage(int from, JToken data) {
+		Debug.Log("OnMessage " + from + " " + data);
+//		AirConsole.instance.Message
+
+		var key = (string)data["key"];
+		var down = (bool)data["pressed"];
+		var keyCode = (KeyCode)System.Enum.Parse(typeof(KeyCode), key);
+		if (down)
+			InputBroker.Instance.ForceKeyDown(keyCode);
+		else
+			InputBroker.Instance.ClearKeyDown(keyCode);
+	}
+}
+
