9dab28ae7856cea4455919759ea1ad4295ecfb5c
Pedro Rabinovitch <pedromr@gmail.com>
2016-09-15 16:26:00 -0400

    Starting the game via controller is now a reality

--- Assets/AncientMech/Scripts/Scenes/StartMenu.cs
+++ Assets/AncientMech/Scripts/Scenes/StartMenu.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using InControl;
 using UnityEngine.SceneManagement;
 using System.Collections;
@@ -8,6 +9,8 @@ public class StartMenu : MonoBehaviour
 {
 	public PlayerUI[] playerUIs;
 
+	public Button StartButton;
+
 	private SessionConfig config;
 
 	// Use this for initialization
@@ -15,6 +18,21 @@ public class StartMenu : MonoBehaviour
 	{
 		config = SessionConfig.Instance;
 		config.numPlayers = 0;
+
+		StartButton.interactable = false;
+		StartButton.onClick.AddListener(StartGame);
+
+//		foreach(var device in InputManager.Devices)
+//		{
+//			Debug.Log("Device " + device.Name);
+//			foreach (var control in device.Controls) {
+//				if (control != null) {
+//					Debug.Log(control.ToString());
+//					Debug.Log(control.Handle);
+//				}
+//			}
+//		}
+
 	}
 	
 	// Update is called once per frame
@@ -22,12 +40,17 @@ public class StartMenu : MonoBehaviour
 	{
 		foreach(var device in InputManager.Devices)
 			CheckDevice(device);
+
+		StartButton.interactable = config.numPlayers > 0;
+
+		if (InputManager.ActiveDevice.MenuWasPressed)
+			StartGame();
 	}
 
 	void CheckDevice(InputDevice device)
 	{
 		//TODO check disconnections
-		if (device.AnyButton) {
+		if (device.AnyButton || device.MenuWasPressed) {
 			var control = GetControlTypeFor(device);
 
 			var alreadyThere = false;
@@ -65,6 +88,9 @@ public class StartMenu : MonoBehaviour
 
 	public void StartGame()
 	{
+		if (config.numPlayers <= 0)
+			return;
+		
 		config.playerControls = new SessionConfig.ControlType[config.numPlayers];
 		for (var i=0; i < config.numPlayers; i++) {
 			config.playerControls[i] = playerUIs[i].control;
